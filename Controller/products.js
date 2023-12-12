0b8c7ab561f4c9135cf658e1290dc79b42f3bf5d
shamon2654 <shamons226@gmail.com>
2023-12-12 21:07:21 +0530

    feat:numericFiltering

--- Controller/products.js
+++ Controller/products.js
@@ -2,12 +2,12 @@ const Product=require("../Models/products")
 
 
 const getAllProductsStatic = async (req, res) => {
-    const products = await Product.find({}).sort("name").select("name price").limit(10).skip(1)
+    const products = await Product.find({price:{$gt:30}}).sort("name").select("name price")
     res.status(200).json({products,nbHits:products.length})
 };
 
 const getAllProducts = async (req, res) => {
-    const { featured, company, name, sort,fields,page,limit } = req.query;
+    const { featured, company, name, sort,fields,page,limit,numericFilter } = req.query;
     console
     const queryObject = {};
     if (featured) {
@@ -19,7 +19,29 @@ const getAllProducts = async (req, res) => {
     if (name) {
         queryObject.name = {$regex: name, $options: "i" }//regex=search function method and filter the value .is called query operator. options is also qu ery operator
     }
+    if (numericFilter) {// only filter number value is called numeric filteraization
+        const operatorMap = {
+            ">": "$gt",
+            ">=": "$gte",
+            "=": "$eq",
+            "<": "$lt",
+            "<=": "$lte",
+        }
+        const reqExp = /\b(>|<|>=|=|<=)\b/g;//which expression  are  avilable inthe reqular expresion denoted
+        let filters = numericFilter.replace(reqExp, (match) => `-${operatorMap[match]}-`);//replace using to replace the value to another
+        console.log(filters)
 
+        const options = ["price", "rate"];//numeric ayitt varunna value kodukunna thane options
+         
+        filters = filters.split(",").forEach((item) => {//forEach using iterating array elements
+            const [field, operator, value] = item.split("-")//destucture array elements
+            if (options.includes(field)) {
+                queryObject[field]={[operator]:Number(value)}
+            }
+        })
+
+    }
+    console.log(queryObject)
 
     // let product = await Product.find(queryObject).sort(sortValue);
     let result =  Product.find(queryObject);
